de13a299a10b86620903081403c516f674397e19
paolaera <52582436+paolaera@users.noreply.github.com>
2020-06-05 17:52:02 +0200

    Risolto utilizzo PV

--- BatteryCharge.m
+++ BatteryCharge.m
@@ -1,28 +1,30 @@
-function [battery,energy] = BatteryCharge(battery,energy,maxCharge,energyPartition)
-      
-%maxCharge15min = maxChargeForStep(maxCharge,SOC);
-%moltiplichiamo per 3/VehiclesIn per essere sicuri di dare un po di energia a tutte le macchine presenti,il 3 serve per non dare troppa poca carica   
-    
-    if (battery + energy) <= maxCharge    %l'energia prodotta ci sta nella batteria
-                if energy <= energyPartition    %l'energia prodotta non supera la costante di carica
-                   battery = battery + energy;
-                   energy = 0;% tutta la carica va nella batteria
-                else             
-                    energy = energy - energyPartition;    
-                    battery= battery + energyPartition;
-                end
-            else
-                if energy <= energyPartition
-                    battery = maxCharge;
-                    energy = energy + battery - maxCharge;
-                else
-                    if battery + energyPartition < maxCharge
-                        battery = battery + energyPartition;
-                        energy = energy - energyPartition;
-                    else
-                        battery = maxCharge;
-                        energy = energy - maxCharge + battery;
-                    end
-                end
-    end
+function [battery,energySales,chargeRemain] = BatteryCharge(battery,energy,maxCharge,percentCharge,energySales,SOC)
+
+maxCharge15minPV = energy * percentCharge;     
+maxCharge15min = maxChargeForStep(maxCharge,SOC);
+if maxCharge15minPV < maxCharge15min
+    chargeRemain = maxCharge15min - maxCharge15minPV;
+    maxCharge15min = maxCharge15minPV;
+    energySales = 0;
+else
+    energySales = energySales + maxCharge15minPV - maxCharge15min;
+    chargeRemain = 0;
 end
+
+if (battery + maxCharge15min) <= maxCharge    %l'energia prodotta ci sta nella batteria
+    battery = battery + maxCharge15min;
+else 
+    battery = maxCharge;
+end
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+               
